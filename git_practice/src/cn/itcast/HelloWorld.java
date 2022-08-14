bd3a83ca5274a7bbf674aa981ae326c31a7e89ac
fei <fei@qq.com>
2022-08-14 19:43:12 +0800

    提交工程到git

---
+++ git_practice/src/cn/itcast/HelloWorld.java
@@ -0,0 +1,11 @@
+package cn.itcast;
+
+/**
+ * @author
+ * @date 2022/8/14 19:28
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("helloworld");
+    }
+}
